b36e4a40e23d86041cbe206890fa196789606828
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-13 20:47:18 +0530

    completd

--- models/patient.js
+++ models/patient.js
@@ -1,7 +1,11 @@
 const mongoose=require("mongoose")
 const schema=mongoose.Schema(
     {
-        "name":""
-        "age":""
+        "name":"",
+        "age":"",
+        "Adress":"",
+        "doctor":"",
     }
 )
+let patientmodel=mongoose.model("patient",schema)
+module.exports={ patientmodel}
